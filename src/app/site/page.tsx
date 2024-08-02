d06086295b56db9117f4c51de3f1e56360181fa2
samrat-p <mitra.stark.68@gmail.com>
2024-08-03 00:05:15 +0530

    added dark-mode

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <section className="h-full w-full pt-[30%] relative flex items-center justify-center flex-col">
+      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
         {/*grid*/}
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
         <p className="text-center">Run your agency, in one place</p>
@@ -19,6 +19,7 @@ export default function Home() {
             width={1200}
             className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
           />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"> {/*darkmode*/}</div>
         </div>
       </section>
     </>
